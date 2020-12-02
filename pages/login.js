9fd5d54ed5371f085e20d24f63c37c17809ae9bf
Tim Ermilov <yamalight@gmail.com>
2020-12-02 16:43:52 +0100

    Apply basic styling to login page

--- pages/login.js
+++ pages/login.js
@@ -1,28 +1,70 @@
 import { csrfToken } from 'next-auth/client';
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Login({ csrfToken }) {
   return (
-    <div className="">
+    <div className="bg-coolGray-100 w-full h-full">
       <Head>
         <title>Task Manager - Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="p-2">
-        <h1 className="font-bold">Login!</h1>
+      <main className="w-full h-full flex flex-col items-center justify-center">
+        <h1 className="text-coolGray-600 mb-2 text-2xl font-bold">
+          Task Manager: Login
+        </h1>
 
-        <form method="post" action="/api/auth/callback/credentials">
+        <form
+          method="post"
+          action="/api/auth/callback/credentials"
+          className="bg-white p-4 shadow sm:rounded-md sm:overflow-hidden"
+        >
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <label>
-            Email
-            <input name="email" type="text" />
-          </label>
-          <label>
-            Password
-            <input name="password" type="text" />
-          </label>
-          <button type="submit">Sign in</button>
+
+          <div className="w-64 mb-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-coolGray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="text"
+              autoComplete="email"
+              className="my-1 p-1 block w-full border border-coolGray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+
+          <div className="w-64 mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-coolGray-700"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="my-1 p-1 block w-full border border-coolGray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+
+          <div className="flex w-full items-center justify-between">
+            <Link href="/register">
+              <a className="text-sm text-coolGray-600">Register</a>
+            </Link>
+
+            <button
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
         </form>
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
+        coolGray: colors.coolGray,
+        indigo: colors.indigo,
         'accent-1': '#333',
       },
       gridTemplateColumns: {
